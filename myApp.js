347587be5007625dc73d6db04f3646873ba06d2c
tainah.carvalho <tainah.carvalho@a3data.com.br>
2025-03-20 08:34:36 -0300

    api config

--- myApp.js
+++ myApp.js
@@ -1,8 +1,22 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+
+app.use('/public', express.static(__dirname + '/public'));
+
+let absolutePath = __dirname + '/views/index.html';
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 app.get('/', function(req, res) {
-  res.send('Hello Express');
+  res.sendFile(absolutePath);
+})
+
+app.get('/json', function(req, res) {
+  res.json({"message": " json"});
 })
 
 module.exports = app;
